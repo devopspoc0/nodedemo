bd76391a8c1d8fd7fcdfbd0eab3a31570b097d74
selvadevan <devopspoc0@gmail.com>
2021-06-20 20:11:26 +0530

    take2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world take 2");
 });
 
 app.listen(PORT, () => {
