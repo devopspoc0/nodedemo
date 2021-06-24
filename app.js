37d729aa65ddfbdfa8e075ca8ab9a7cc7f2da400
selvadevan <devopspoc0@gmail.com>
2021-06-24 08:51:46 +0530

    Commit with ngork..

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world with ngork take 2");
+  res.send("Hello world with ngork");
 });
 
 app.listen(PORT, () => {
