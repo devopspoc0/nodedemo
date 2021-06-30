2d99f666e9d1ff20c06bfeb9172f60ab6aa749fc
devopspoc0 <86120188+devopspoc0@users.noreply.github.com>
2021-06-30 18:45:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world with ngork aws");
+  res.send("Hello world with ngork aws from github");
 });
 
 app.listen(PORT, () => {
