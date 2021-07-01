8956ea1b25dcbdd1f9d1cb03ece63d14f7e22d86
DevOps POC <devopspoc0@gmail.com>
2021-07-01 07:25:13 +0000

    Commit with ngork aws take 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world with ngork aws and take 2");
+  res.send("Hello world with ngork aws and take 3");
 });
 
 app.listen(PORT, () => {
